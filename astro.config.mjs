56d5d7d672c58bc9b5013db19febb47625123b6a
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2025-12-07 23:01:27 +0100

    fix: define config site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://ferriskey.io',
+  site: 'https://ferriskey.rs',
   vite: {
     plugins: [tailwindcss()],
   },
